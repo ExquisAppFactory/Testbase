aebf22371147415c1ab589c6bab81199271691e6
bolajahmad <56865496+bolajahmad@users.noreply.github.com>
2021-09-20 17:53:15 +0100

    updated

--- paymentService/src/routes/index.ts
+++ paymentService/src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getBankList, verifyPaymentController } from '../controllers';
+import { fetchTransactions, getBankList, verifyPaymentController } from '../controllers';
 
 const router = Router();
 
 router.get('/banks', getBankList);
+router.get('/transactions', fetchTransactions);
 router.post('/verify', verifyPaymentController);
 
 export default router;
